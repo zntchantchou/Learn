6cfef25001bb38f31de9fde10d2e93920436e408
zacharie.ntchantchou <zacharie.ntchantchou@ouido.fr>
2024-02-02 11:14:05 +0100

    Lists done

--- _learn/ds-algo/lists/aside/kiosk.js
+++ _learn/ds-algo/lists/aside/kiosk.js
@@ -3,6 +3,7 @@ import { createArr } from "./readfile";
 
 const movies = await createArr("./movies.txt");
 const moviesList = new List();
+const rentedList = new List();
 for (let i = 0; i < movies.length; i++) {
   moviesList.append(movies[i]);
 }
@@ -38,17 +39,43 @@ function displayList(list) {
 }
 
 function checkoutMovie(name, movie, moviesList, customersList) {
-  const movieName = movie.toUpperCase();
+  const movieName = movie.toUpperCase().trim();
+  console.log('movie ', movie);
   if (moviesList.contains(movieName)) {
     console.log("[checkOutMovie] ");
     const customer = new Customer(name, movieName);
+    rentedList.append(movieName)
     moviesList.remove(movieName);
     customersList.append(customer);
+    console.log("------------ Movies rented currently ------------ ");
+    displayList(rentedList)
   } else {
-    console.log("Movie not available");
+    console.log("Movie ", movieName, " not available");
+    console.log(moviesList.contains(movieName));
   }
 }
 
+
+function returnMovie(movie) {
+  // check if rented
+  // if rented delete from list and add to moviesList
+  const movieName = movie.toUpperCase().trim();
+  if(!rentedList.contains(movieName)) {
+    console.log(movieName, ': is not rented currently');
+    return;
+  } 
+  moviesList.append(movieName)
+  rentedList.remove(movieName);
+  console.log('MOVIE RETURNED');
+}
+
 checkoutMovie("jack", "Star WaRs", moviesList, customersList);
-displayList(customersList);
-displayList(moviesList);
+checkoutMovie("Prava", "Fight Club", moviesList, customersList);
+checkoutMovie("Steph", "GoodFELLAS ", moviesList, customersList);
+checkoutMovie("Steph", "CITY OF GOD", moviesList, customersList);
+
+// returnMovie('GOODFELLAS');
+// returnMovie('BOUM');
+returnMovie('CITy of God');
+
+console.log('MOVIES ', moviesList.toString())
