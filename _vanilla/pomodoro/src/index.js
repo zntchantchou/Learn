b56f285ed36b7debd703cd2778fcebd94f93f232
zacharie.ntchantchou <zacharie.ntchantchou@ouido.fr>
2024-02-03 02:39:21 +0100

    what

--- _vanilla/pomodoro/src/index.js
+++ _vanilla/pomodoro/src/index.js
@@ -1,3 +1,4 @@
+import Storage from "./Storage.js";
 import { millisecondsToMinsAndSecs } from "./utils.js";
 
 const statuses = Object.freeze({
@@ -6,38 +7,61 @@ const statuses = Object.freeze({
   COUNTING: "COUNTING",
 });
 
+const DEFAULT_TIMER = {
+  name: "Default",
+  totalTime: 10000,
+  timeLeft: 1000,
+}
+
 const countdownElt = document.getElementById("countdown");
 const startBtnElt = document.getElementById("start-pause-btn");
 const progressCircleElt = document.getElementById("circle-progress");
 
 const CIRCLE_RADIUS = 45;
 const circumference = 2 * CIRCLE_RADIUS * Math.PI;
-const minutes = 1 / 4;
+const minutes = 0;
 const seconds = minutes * 60;
-const milliseconds = seconds * 1000;
+let milliseconds = seconds * 1000;
 let timeLeftInMs = milliseconds;
 let intervalId;
 let status = statuses.STOPPED;
 
-/** Size of the step in the progress circle each second */
-const tickOffset = circumference / seconds;
+/** Size of the step in the progress circle each second */ 
+function getTickOffset() {
+  return circumference / (storage.getCurrentTimer().totalTime / 1000);
+}
 
 function click() {
   console.log("[CLICK]");
-  if ([statuses.PAUSED, statuses.STOPPED].includes(status)) {
+  if (status === statuses.COUNTING) {
+    pause();
+  } else {
     start();
-    return;
   }
-  pause();
 }
 
-function reset() {
+function getTimeElapsed() {
+  const currentTimer = storage.getCurrentTimer();
+  let result = 0;
+  let hasDecremented = currentTimer && currentTimer.totalTime > currentTimer.timeLeft
+  if(hasDecremented) {
+    result = (currentTimer.totalTime - currentTimer.timeLeft) / 1000;
+  }
+  return result;
+}
+
+
+function resetCounter() {
   console.log("[RESET]");
   startBtnElt.innerHTML = "start";
-  countdownElt.innerHTML = millisecondsToMinsAndSecs(milliseconds);
-  timeLeftInMs = milliseconds;
+  const timeLeft = storage.getCurrentTimer().timeLeft;
+  const tickOffset = getTickOffset();
+  timeLeftInMs = timeLeft;
+  countdownElt.innerHTML = millisecondsToMinsAndSecs(timeLeft);
+  console.log('TIME ELAPSED', getTimeElapsed())
+  console.log('Tick offset', tickOffset);
   progressCircleElt.style.strokeDasharray = circumference;
-  progressCircleElt.style.strokeDashoffset = 0;
+  progressCircleElt.style.strokeDashoffset = -1* tickOffset * getTimeElapsed();
   progressCircleElt.style.stroke = "rgb(186, 73, 73)";
   progressCircleElt.style.visibility = "visible";
   // disable animation when resetting
@@ -70,7 +94,7 @@ function stop() {
   status = statuses.STOPPED;
   clearInterval(intervalId);
   progressCircleElt.style.visibility = "hidden";
-  reset();
+  resetCounter();
 }
 
 function tick() {
@@ -80,7 +104,10 @@ function tick() {
     console.log("< 1000");
   }
   timeLeftInMs -= 1000;
+
   countdownElt.innerHTML = millisecondsToMinsAndSecs(timeLeftInMs);
+  storage.updateCurrentTimer({...storage.getCurrentTimer(), timeLeft: timeLeftInMs})
+
   if (timeLeftInMs <= 0) {
     console.log("none");
     stop();
@@ -88,10 +115,11 @@ function tick() {
     return;
   }
   let currentOffset = Number(progressCircleElt.style.strokeDashoffset);
-  currentOffset -= tickOffset;
+  currentOffset -= getTickOffset();
   progressCircleElt.style.strokeDashoffset = currentOffset;
 }
 
 // START
 startBtnElt.addEventListener("click", click);
-reset();
+const storage = new Storage();
+resetCounter();
