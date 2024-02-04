3766b468f663c0ca514d6ec5b08a0fc433421eda
zntchantchou <z.ntchantchou@gmail.com>
2024-02-04 01:33:31 +0100

    overlay

--- _vanilla/pomodoro/app/CreateButton.js
+++ _vanilla/pomodoro/app/CreateButton.js
@@ -2,13 +2,36 @@ function CreateButton() {
   this.anchorElt = document.getElementById("root");
   this.element;
   this.draw = draw;
-  console.log("hellow");
+  this.listen = listen;
+  this.handleClickCreate = handleClickCreate.bind(this);
+  this.handleClickAway = handleClickAway.bind(this);
+  const overlayElt = document.getElementById("overlay");
+
+  function listen() {
+    this.element.addEventListener("click", this.handleClickCreate);
+    overlayElt.addEventListener("click", this.handleClickAway);
+  }
+
   function draw() {
-    console.log("CREATE BTN DRAW");
+    console.log("CreateBtn [DRAW]");
     this.element = document.createElement("div");
     this.element.id = "create-btn";
     this.element.textContent = "+";
+    // arrow function can be used as well
     this.anchorElt.appendChild(this.element);
+    this.listen();
+  }
+
+  function handleClickCreate(e) {
+    console.log("[handleClickCreate]", overlayElt);
+    e.preventDefault();
+    overlayElt.style.visibility = "visible";
+  }
+
+  function handleClickAway(e) {
+    console.log("[handleClickAway]", overlayElt, e);
+    e.preventDefault();
+    overlayElt.style.visibility = "hidden";
   }
 }
 
